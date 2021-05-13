b2608b901332e367c6924b535e501fed49226f9b
Daniel Smith <72361870+CaptainDestructo@users.noreply.github.com>
2021-05-12 20:29:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from time import sleep
+
+from Classes.Board import Board
+
+
+board = Board()
+window = board.window
+last_piece = None
+while True:
+    click = window.getMouse()
+    piece, location = board.onclick(click)
+    if piece == None:
+        if last_piece:
+            last_piece.clear_moves()
+        last_piece = None
+        continue
+    elif piece.location == location:
+        if piece == last_piece:
+            last_piece.clear_moves()
+            last_piece = None
+        else:
+            piece.onclick()
+        last_piece = piece
